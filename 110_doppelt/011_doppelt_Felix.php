2d37654eaa5347c5825123cd62db700a44ac1bff
FeLahme <fortressofsolitude@gmx.de>
2025-09-23 08:45:57 +0200

    Add files via upload

---
+++ 110_doppelt/011_doppelt_Felix.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Doppelt
+ *
+ * Schreibe eine Funktion, die überprüft,
+ * ob in einem Array keine doppelten Elemente sind.
+ * Der Funktion wird das Array übergeben
+ * und sie soll true oder false zurückgeben.
+ *
+ */
+$array1 = [1, 2, 3, 1];
+$array2 = [1, 2, 3, 4];
+
+function checkUnique($array) {
+    $unique = array_unique($array);
+    if (count($array) === count($unique)) {
+        echo "true";
+    } else {
+        echo "false";
+    }
+}
+checkUnique($array1);
+echo "<br>";
+checkUnique($array2);
