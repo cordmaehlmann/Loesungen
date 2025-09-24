364349ea5cf461be40a38c62e8ec720f9f3433d2
OnderBektas1992 <onder_bektas@hotmail.com>
2025-09-24 20:53:59 +0200

    Create 120_Onder.php

---
+++ 120_einmaliges_im_array/120_Onder.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Einmaliges im Array
+ *
+ * Schreibe ein Programm, das ein Array mit neun Zahlen befüllt.
+ * Dabei sollen vier Zahlen doppelt vorkommen
+ * und eine Zahl nur einmal.
+ *
+ * Schreibe dann ein Programm, das aus diesem Array die Zahl findet,
+ * die nur einmal vorkommt.
+ */
+
+$doppelteZahlen=[];
+$einzigeZahl=[];
+$a=0;
+while($a<4) {
+    $randomZahl=rand(1,99);
+    if(!in_array($randomZahl,$doppelteZahlen)) {
+        array_push($doppelteZahlen, $randomZahl, $randomZahl);
+        $a++;
+    }
+}
+for($i=0;$i<1;$i++) {
+    $randomZahl=rand(0,99);
+    if(!in_array($randomZahl,$doppelteZahlen)){
+        array_unshift($einzigeZahl,$randomZahl);
+    }else {
+        $i--;
+    }
+}
+$myArray= array_merge($doppelteZahlen,$einzigeZahl);
+shuffle($myArray);
+var_dump($myArray);
+$count = array_count_values($myArray);
+$suchendeZahl = array_search(1, $count);
+echo $suchendeZahl." wurde nur einmal in der Liste verwendet!";
